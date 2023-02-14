266f6429a15a54578fe87c26c69e8c526cae072d
akhila.mp <akhila.mp@AKHILAMP>
2023-02-14 16:17:37 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+string input = "AKHILA"; 
+char[] charArray = new char[input.Length];
+int j = input.Length - 1;
+for (int i = 0; i < input.Length; i++) 
+{
+    charArray[i] = input[j];
+    j--; 
+}
+string reversed = new string(charArray);
+Console.WriteLine(input);
+Console.WriteLine(reversed); 
+
